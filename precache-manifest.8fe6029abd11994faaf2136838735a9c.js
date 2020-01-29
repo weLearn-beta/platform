c4f2ca92d4c0d766c09cda8a574538559c0b8be2
Lucky Rathore <luckyrathore03@gmail.com>
2020-01-29 23:36:55 +0530

    Updates

---
+++ precache-manifest.8fe6029abd11994faaf2136838735a9c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c04bbe425766f14e1905c4080113e18",
+    "url": "/platform/index.html"
+  },
+  {
+    "revision": "be394a9285bb0970f020",
+    "url": "/platform/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "e35b2d2f416419a873e3",
+    "url": "/platform/static/js/2.848b290d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/platform/static/js/2.848b290d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "be394a9285bb0970f020",
+    "url": "/platform/static/js/main.ed77a808.chunk.js"
+  },
+  {
+    "revision": "5b7ad3de3bb15a0628e5",
+    "url": "/platform/static/js/runtime-main.55bd58e3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/platform/static/media/logo.5d5d9eef.svg"
+  }
+]);
